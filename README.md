01e7b8cd68ea1fe3687ab5df29942c6651eaa3be
nikoskanel92 <nikoskanel@hotmail.com>
2022-08-28 22:03:14 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-tgouw4
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-tgouw4)
---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, VERSION } from '@angular/core';
+import { Todo } from './Todo';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  todos : Todo[] = [];
+  newTodo:string;
+
+
+  saveTodo(){
+    if(this.newTodo){
+      let todo = new Todo();
+      todo.name = this.newTodo;
+      todo.isCompleted = true;
+      this.todos.push(todo);
+      this.newTodo.valueOf() - ''.valueOf();
+    }else{
+      alert('Please enter Todo')
+    }
+  }
+}
